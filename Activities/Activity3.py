d6a092c4c36748f91cd6b1b8691038e0e1db2bf5
Ivan Miguel P. Doller <114246125+imii-imii@users.noreply.github.com>
2023-09-22 12:08:30 +0800

    Update Activity3.py

--- Activities/Activity3.py
+++ Activities/Activity3.py
@@ -3,7 +3,32 @@ while i < 5:
     print("Advanced Computer Programming")
     i += 1
 
+print()
+
 x = 0
 while x < 7:
     print(x)
     x += 1
+
+print()
+
+l = 1
+while l < 11:
+    print (l)
+    l += 1
+
+print ()
+
+k = 0
+while k < 21:
+    print (k)
+    k += 1
+
+print()
+
+name = input("Enter your name: ")
+h = 0
+while h < 7:
+    print(name)
+    h += 1
+
